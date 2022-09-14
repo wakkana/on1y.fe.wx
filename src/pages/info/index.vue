ebf8571d3077067cbe66cb66249c86adc2ae4940
wakkana <wakkana@outlook.com>
2022-09-15 00:00:57 +0800

    userspace

--- src/pages/info/index.vue
+++ src/pages/info/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div class="user-space">
+    <div id="username">
+      username
+    </div>
   </div>
 </template>
 
@@ -9,5 +12,20 @@ export default {}
 </script>
 
 <style scoped>
+.user-space {
+  background-color: aqua;
+  width: auto;
+  height: 25vh;
+  display: flex;
+  flex-direction: row;
+  border-radius: 10px;
+  margin: 25px 20px 25px 20px;
+  padding: 30px 50px 0px 50px;
+  background-image: linear-gradient(to bottom right, red, yellow);
+}
 
+#username {
+  width: 300px;
+  height: 10vh;
+}
 </style>
